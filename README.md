94f2adade974ce8298f4396d77932e840d3489d1
Zachary Huff <zach.huff.386@gmail.com>
2015-07-26 20:58:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# httpserver
+
+Http file server written with Go using directory listing similar to apache
+
+```
+$ go get github.com/pacur/httpserver
+$ httpserver
+Listening and serving /home/user on :8000
+```
---
+++ main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+const body = `<html>
+<head><title>Index of %s</title></head>
+<body bgcolor="white">
+<h1>Index of %s</h1><hr><pre><a href="../">../</a>
+%s</pre><hr></body>
+</html>
+`
+
+func IsDirectory(path string) (dir bool, err error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = nil
+		}
+		return
+	}
+
+	dir = stat.IsDir()
+	return
+}
+
+type Item struct {
+	Name      string
+	IsDir     bool
+	Formatted string
+}
+
+type Items struct {
+	items []Item
+}
+
+func (s *Items) Len() (n int) {
+	n = len(s.items)
+	return
+}
+
+func (s *Items) Less(i int, j int) bool {
+	if s.items[i].IsDir && !s.items[j].IsDir {
+		return true
+	}
+	if s.items[i].Name[:1] == "." && s.items[i].Name[:1] != "." {
+		return true
+	}
+	return s.items[i].Name < s.items[j].Name
+}
+
+func (s *Items) Swap(i int, j int) {
+	s.items[i], s.items[j] = s.items[j], s.items[i]
+}
+
+func (s *Items) Add(item Item) {
+	s.items = append(s.items, item)
+}
+
+func (s *Items) Sort() {
+	sort.Sort(s)
+}
+
+func (s *Items) Join(sep string) (data string) {
+	for i, item := range s.items {
+		if i != 0 {
+			data += sep
+		}
+		data += item.Formatted
+	}
+	return
+}
+
+type StaticHandler struct {
+	Root       string
+	fileServer http.Handler
+}
+
+func (h *StaticHandler) Handle(c *gin.Context) {
+	path := filepath.Join(h.Root, filepath.FromSlash(
+		filepath.Clean("/"+c.Param("filepath"))))
+
+	isDir, err := IsDirectory(path)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	ok := false
+	if isDir {
+		ok, err = h.HandleDirList(path, c)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+	}
+
+	if !ok {
+		h.fileServer.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func (h *StaticHandler) HandleDirList(path string, c *gin.Context) (
+	ok bool, err error) {
+
+	pathFrm := filepath.Clean("/" + c.Param("filepath"))
+	if pathFrm[len(pathFrm)-1:] != "/" {
+		pathFrm += "/"
+	}
+
+	items := &Items{}
+
+	itemsAll, err := ioutil.ReadDir(path)
+	if err != nil {
+		return
+	}
+
+	for _, item := range itemsAll {
+		name := item.Name()
+		if name == "index.html" {
+			return
+		}
+
+		modTime := item.ModTime().Format("02-Jan-2006 15:04")
+
+		if item.Mode()&os.ModeSymlink != 0 {
+			linkPath, e := os.Readlink(filepath.Join(path, item.Name()))
+			if e != nil {
+				err = e
+				return
+			}
+
+			itm, e := os.Lstat(linkPath)
+			if e != nil {
+				if os.IsNotExist(e) {
+					continue
+				}
+				err = e
+				return
+			}
+			item = itm
+		}
+
+		size := ""
+		if item.IsDir() {
+			name += "/"
+			size = "-"
+		} else {
+			size = fmt.Sprintf("%d", item.Size())
+		}
+
+		formattedName := name
+		if len(formattedName) > 50 {
+			formattedName = formattedName[:47] + "..>"
+		}
+
+		items.Add(Item{
+			Name:  name,
+			IsDir: item.IsDir(),
+			Formatted: fmt.Sprintf(
+				`<a href="%s">`, name) + fmt.Sprintf(
+				"%-54s %s % 19s", formattedName+"</a>", modTime, size),
+		})
+	}
+
+	items.Sort()
+
+	ok = true
+	data := []byte(fmt.Sprintf(body, pathFrm, pathFrm, items.Join("\n")))
+	c.Data(200, "text/html", data)
+
+	return
+}
+
+func (h *StaticHandler) Setup(engine *gin.Engine) {
+	fs := gin.Dir(h.Root, false)
+	h.fileServer = http.StripPrefix("/", http.FileServer(fs))
+
+	engine.GET("/*filepath", h.Handle)
+	engine.HEAD("/*filepath", h.Handle)
+
+	return
+}
+
+func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	path = *flag.String("path", path, "Path to serve")
+	host := *flag.String("host", "", "Server host")
+	port := *flag.Int("port", 8000, "Server port number")
+	flag.Parse()
+
+	path, err = filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+
+	static := &StaticHandler{
+		Root: path,
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	static.Setup(router)
+
+	fmt.Printf("Listening and serving %s on %s:%d\n", path, host, port)
+
+	err = router.Run(fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		panic(err)
+	}
+}
